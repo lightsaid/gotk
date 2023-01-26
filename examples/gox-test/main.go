1a4ad4a74dd1059d7125f33461f144f3d7960dab
lightsaid <lightsaid@163.com>
2023-01-26 10:59:01 +0800

    gox 添加 http methods -2

--- examples/gox-test/main.go
+++ examples/gox-test/main.go
@@ -9,22 +9,22 @@ import (
 
 func main() {
 	mux := gox.New()
-	mux.HandleFunc("/", indexHandle)
-	mux.HandleFunc("/tag", tagHandle)
-	mux.HandleFunc("/post", postHandle)
+
+	mux.GET("/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "get")
+	})
+
+	mux.POST("/post", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "post")
+	})
+
+	mux.DELETE("/delete", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "delete")
+	})
+
+	mux.PUT("/put", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "put")
+	})
 
 	http.ListenAndServe("0.0.0.0:9999", mux)
-
-}
-
-func indexHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "indexHandle")
-}
-
-func tagHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "tagHandle")
-}
-
-func postHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "postHandle")
 }
--- gox/gox.go
+++ gox/gox.go
@@ -8,20 +8,47 @@ import (
 
 // gox 是一个简单的路由实现, 参考了 http.NewServeMux
 
+/*
+
+package http
+
+// Common HTTP methods.
+//
+// Unless otherwise noted, these are defined in RFC 7231 section 4.3.
+const (
+	MethodGet     = "GET"
+	MethodHead    = "HEAD"
+	MethodPost    = "POST"
+	MethodPut     = "PUT"
+	MethodPatch   = "PATCH" // RFC 5789
+	MethodDelete  = "DELETE"
+	MethodConnect = "CONNECT"
+	MethodOptions = "OPTIONS"
+	MethodTrace   = "TRACE"
+)
+
+*/
+
+var SupportMethods = []string{
+	http.MethodGet, http.MethodHead, http.MethodPost,
+	http.MethodPut, http.MethodPatch, http.MethodDelete,
+	http.MethodConnect, http.MethodOptions, http.MethodTrace}
+
 type Gox struct {
 	mutex  sync.RWMutex
-	routes map[string]Route
+	routes map[string]route
 }
 
-type Route struct {
+type route struct {
 	h       http.Handler
+	methods []string
 	pattern string
 }
 
 func New() *Gox {
 	return &Gox{
 		mutex:  sync.RWMutex{},
-		routes: make(map[string]Route),
+		routes: make(map[string]route),
 	}
 }
 
@@ -32,18 +59,75 @@ func (gx *Gox) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if len(route.methods) == 0 {
+		route.methods = SupportMethods
+	}
+
+	exists := has(route.methods, r.Method)
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
 	route.h.ServeHTTP(w, r)
 }
 
-func (gx *Gox) HandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
-	if handler == nil {
-		panic("http: nil handler")
+func has(ss []string, s string) bool {
+	for _, v := range ss {
+		if v == s {
+			return true
+		}
 	}
+	return false
+}
+
+func (gx *Gox) GET(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodGet)
+}
+
+func (gx *Gox) HEAD(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodHead)
+}
+
+func (gx *Gox) POST(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodPost)
+}
+
+func (gx *Gox) PUT(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodPut)
+}
+
+func (gx *Gox) PATCH(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodPatch)
+}
+
+func (gx *Gox) DELETE(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodDelete)
+}
+
+func (gx *Gox) CONNECT(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodConnect)
+}
+
+func (gx *Gox) OPTIONS(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodOptions)
+}
+
+func (gx *Gox) TRACE(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	gx.Handle(pattern, http.HandlerFunc(handler), http.MethodTrace)
+}
+
+func (gx *Gox) HandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	// 交给 gx.Handle 验证
+	// if handler == nil {
+	// 	panic("http: nil handler")
+	// }
 
 	gx.Handle(pattern, http.HandlerFunc(handler))
 }
 
-func (gx *Gox) Handle(pattern string, handler http.Handler) {
+// Handle 添加路由/路由注册
+func (gx *Gox) Handle(pattern string, handler http.Handler, methods ...string) {
 	gx.mutex.Lock()
 	defer gx.mutex.Unlock()
 
@@ -60,9 +144,10 @@ func (gx *Gox) Handle(pattern string, handler http.Handler) {
 	}
 
 	if gx.routes == nil {
-		gx.routes = make(map[string]Route)
+		gx.routes = make(map[string]route)
 	}
-	r := Route{h: handler, pattern: pattern}
+
+	r := route{h: handler, pattern: pattern, methods: methods}
 	gx.routes[pattern] = r
 }
 
