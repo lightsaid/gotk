8f3d8f70fe07a5812ec035e481a0001f5561a8aa
lightsaid <lightsaid@163.com>
2023-02-05 23:55:32 +0800

    gox 路由中间件实现

--- examples/gox-test/main.go
+++ examples/gox-test/main.go
@@ -1,25 +1,86 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/lightsaid/gotk/gox"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Printf("%s - %s %s %s\n", r.RemoteAddr, r.Proto, r.Method, r.URL)
+
+		w.Header().Set("abc", "abc")
+		ctx := context.WithValue(r.Context(), gox.ContextKey("fff"), "hello fff")
+		fmt.Println("<<<<< AAA >>>>>")
+
+		// rad := rand.Intn(10)
+		// if rad > 5 {
+		// 	next.ServeHTTP(w, r.WithContext(ctx))
+		// }
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func bbbRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("<<<<< BBB >>>>>")
+		fmt.Println("bbb result: ", w.Header().Get("abc"), r.Context().Value(gox.ContextKey("fff")))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func cccRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("<<<<< CCC >>>>>")
+		fmt.Println("ccc result: ", w.Header().Get("abc"), r.Context().Value(gox.ContextKey("fff")))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func dddRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("<<<<< DDD >>>>>")
+		fmt.Println("ddd result: ", w.Header().Get("abc"), r.Context().Value(gox.ContextKey("fff")))
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	mux := gox.New()
 
-	mux.GET("/get1", func(w http.ResponseWriter, r *http.Request) {
+	mux.Register("AAA", logRequest)
+	mux.Register("BBB", bbbRequest)
+	mux.Register("CCC", cccRequest)
+	mux.Register("DDD", dddRequest)
+
+	mux.Global("AAA", "BBB")
+
+	mux.GET("/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("result: ", w.Header().Get("abc"), r.Context().Value(gox.ContextKey("fff")))
 		fmt.Fprintf(w, "get")
+		fmt.Println("完成请求～～")
 	})
 
 	mux.POST("/post", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "post")
-	})
+		fmt.Println("post 请求完成～～")
+	}).Before("CCC").After("DDD")
 
 	mux.DELETE("/delete", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "delete")
+		fmt.Println("post 完成请求～～")
 	})
 
 	mux.PUT("/put", func(w http.ResponseWriter, r *http.Request) {
